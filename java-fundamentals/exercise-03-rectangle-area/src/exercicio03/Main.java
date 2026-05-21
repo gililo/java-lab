614f7c45c25dcee0012c64e3e00bcb23c5ef58cf
Gislaine Lilo <gislaine.lilo@gmail.com>
2026-05-21 18:38:51 -0400

    feat: Add RectangleAreaCalculator

---
+++ java-fundamentals/exercise-03-rectangle-area/src/exercicio03/Main.java
@@ -0,0 +1,23 @@
+package exercicio03;
+
+import exercise02.SquareAreaCalculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Type the base length of the rectangle: ");
+        double baseLength = input.nextDouble();
+
+        System.out.print("Type the height of the rectangle: ");
+        double height = input.nextDouble();
+
+        RectangleAreaCalculator calculator = new RectangleAreaCalculator();
+
+        double area = calculator.calculateArea(baseLength, height);
+
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
---
+++ java-fundamentals/exercise-03-rectangle-area/src/exercicio03/RectangleAreaCalculator.java
@@ -0,0 +1,7 @@
+package exercicio03;
+
+public class RectangleAreaCalculator {
+    public double calculateArea(double baseLength, double height) {
+        return baseLength * height;
+    }
+}
